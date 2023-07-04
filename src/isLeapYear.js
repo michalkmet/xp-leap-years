0d94085bfd1f57bf1adb759c7fa59e0b6408a0c4
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-07-04 13:37:13 +0200

    fifth test pass

--- src/isLeapYear.js
+++ src/isLeapYear.js
@@ -1,5 +1,10 @@
 function isLeapYear(year) {
-  if (year === 4 || year === 400){
+  
+  if (year % 100 === 0 && year % 400 != 0){
+    return false;
+  }
+  
+  if ((year % 4 === 0 || year % 400 === 0) ){
     return true;
   }
   return false;
