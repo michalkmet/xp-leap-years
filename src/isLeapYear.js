33a4b5a4ba9e30af492fade59a344fb35c800a57
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-07-04 13:18:35 +0200

    initial setup

---
+++ src/isLeapYear.js
@@ -0,0 +1,5 @@
+function isLeapYear() {
+  return 'isLeapYear';
+}
+
+module.exports = isLeapYear;
---
+++ src/isLeapYear.test.js
@@ -0,0 +1,7 @@
+const isLeapYear = require('./isLeapYear');
+
+describe('isLeapYear', () => {
+  it('should return isLeapYear', () => {
+    expect(isLeapYear()).toBe('isLeapYear');
+  });
+});
